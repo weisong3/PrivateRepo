6b0cc695c4fa8c5393890035b9ef97829e18237b
Wei Song <weisong3@gmail.com>
2013-05-06 11:03:35 -0700

    update

--- SNS_Sample/SNS_Sample/src/com/sns_sample/LoginActivity.java
+++ SNS_Sample/SNS_Sample/src/com/sns_sample/LoginActivity.java
@@ -1,19 +1,34 @@
 package com.sns_sample;
 
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
+import android.text.Editable;
 
-public class LoginActivity extends BaseActivity {
+import com.sns_sample.dialog.LoginDialogFactory;
+import com.sns_sample.dialog.LoginDialogHandler;
+import com.sns_sample.dialog.LoginDialogResult;
 
-    public LoginActivity() {
-		super(R.string.app_name);
-	}
-
+public class LoginActivity extends FragmentActivity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        
+        LoginDialogHandler handler = new LoginDialogFactory().createDialogHandler(this, "login fragment", new LoginDialogResult() {
+			
+			@Override
+			public void onSignIn(int id, Editable username, Editable password) {
+				MainActivity.startActivity(username.toString(), LoginActivity.this);
+			}
+			
+			@Override
+			public void onRegister(int id) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        LoginDialogFactory.showDialog(handler, getResources().getString(R.string.login_title), null, "", 0);
     }
 
 }
